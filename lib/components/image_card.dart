207c48657a832d72a24bf21ab81134b3c85db1f4
zijianlin <1031531798@qq.com>
2023-09-22 00:20:34 +0800

    feat(滚动): 构建平行错位滚动页面

--- lib/components/image_card.dart
+++ lib/components/image_card.dart
@@ -1,13 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ImageCardType {
   late String url;
   late String name;
   late String country;
 }
+
 class ImageCard extends StatelessWidget {
-  const ImageCard({
+  ImageCard({
     super.key,
     required this.url,
     required this.name,
@@ -16,7 +20,7 @@ class ImageCard extends StatelessWidget {
   final String url;
   final String name;
   final String country;
-
+  final GlobalKey _backgroundImageKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -24,51 +28,65 @@ class ImageCard extends StatelessWidget {
       child: AspectRatio(
         aspectRatio: 16 / 9,
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
+          borderRadius: BorderRadius.circular(16),
           child: Stack(
-            children: [
-              _imageBg(),
-              _gradient(),
-            ],
+            children: [_imageBg(context), _gradient(), _imageInfo()],
           ),
         ),
       ),
     );
   }
 
-  Widget _imageBg () {
+  Widget _imageBg(context) {
     return Positioned.fill(
-      child: Image.network(
-        url,
-        fit: BoxFit.cover,
+      child: Flow(
+        delegate: ParallaxFlowDelegate(
+          scrollable: Scrollable.of(context),
+          listItemContext: context,
+          backgroundImageKey: _backgroundImageKey,
+        ),
+        children: [
+          Image.network(
+            url,
+            key: _backgroundImageKey,
+            fit: BoxFit.cover,
+          )
+        ],
       ),
     );
   }
-  Widget _gradient () {
-    return Positioned(
+
+  Widget _gradient() {
+    return Positioned.fill(
       child: DecoratedBox(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              stops: const [0.6, 0.95],
-            ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.6, 0.95],
           ),
+        ),
       ),
     );
   }
-  Widget _imageInfo () {
-    return Positioned.fill(
+
+  Widget _imageInfo() {
+    return Positioned(
       left: 20,
       bottom: 20,
       child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(name, style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          )),
+          Text(
+            name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           Text(
             country,
             style: const TextStyle(
@@ -80,4 +98,56 @@ class ImageCard extends StatelessWidget {
       ),
     );
   }
+}
+
+class ParallaxFlowDelegate extends FlowDelegate {
+  ParallaxFlowDelegate({
+    required this.scrollable,
+    required this.listItemContext,
+    required this.backgroundImageKey,
+  }) : super(repaint: scrollable.position);
+  final ScrollableState scrollable;
+  final BuildContext listItemContext;
+  final GlobalKey backgroundImageKey;
+
+  @override
+  BoxConstraints getConstraintsForChild(int i, BoxConstraints constraints) {
+    return BoxConstraints.tightFor(
+      width: constraints.maxWidth,
+    );
+  }
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    // TODO: implement paintChildren
+    final scrollableBox = scrollable.context.findRenderObject();
+    final listItemBox = listItemContext.findRenderObject() as RenderBox;
+    final listItemOffset = listItemBox.localToGlobal(
+        listItemBox.size.centerLeft(Offset.zero),
+        ancestor: scrollableBox);
+    final viewportDimension = scrollable.position.viewportDimension;
+    final scrollFraction =
+        (listItemOffset.dy / viewportDimension).clamp(0.0, 1.0);
+    final verticalAlignment = Alignment(0.0, scrollFraction * 2 - 1);
+    final backgroundSize =
+        (backgroundImageKey.currentContext!.findRenderObject() as RenderBox)
+            .size;
+    final listItemSize = context.size;
+    final childRect =
+        verticalAlignment.inscribe(backgroundSize, Offset.zero & listItemSize);
+
+    context.paintChild(
+      0,
+      transform:
+          Transform.translate(offset: Offset(0.0, childRect.top )).transform,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant ParallaxFlowDelegate oldDelegate) {
+    return scrollable != oldDelegate.scrollable ||
+        listItemContext != oldDelegate.listItemContext ||
+        backgroundImageKey != oldDelegate.backgroundImageKey;
+  }
+
 }
--- lib/view/food.dart
+++ lib/view/food.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:words_record/components/image_card.dart';
 
@@ -11,65 +10,56 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPage extends State<FoodPage> {
-  List<Map> foodList = [
-    {
-      "url":
-          "https://media.istockphoto.com/id/1277719283/cs/fotografie/pouli%C4%8Dn%C3%AD-j%C3%ADdlo-burgery-z-masn%C3%BDch-%C5%99%C3%ADzk%C5%AF-jsou-v-pap%C3%ADrov%C3%BDch-krabic%C3%ADch-rozvoz-j%C3%ADdla.jpg?s=612x612&w=0&k=20&c=Gc_EU3B-uy9SPgpNfdh7TEm0awYIJPTGoV-UeodTQGo=",
-      "name": 'check ',
-      "country": 'usa'
-    },
-    {
-      "url":
-          "https://media.istockphoto.com/id/1277719283/cs/fotografie/pouli%C4%8Dn%C3%AD-j%C3%ADdlo-burgery-z-masn%C3%BDch-%C5%99%C3%ADzk%C5%AF-jsou-v-pap%C3%ADrov%C3%BDch-krabic%C3%ADch-rozvoz-j%C3%ADdla.jpg?s=612x612&w=0&k=20&c=Gc_EU3B-uy9SPgpNfdh7TEm0awYIJPTGoV-UeodTQGo=",
-      "name": 'check ',
-      "country": 'usa'
-    },
-    {
-      "url":
-          "https://media.istockphoto.com/id/1277719283/cs/fotografie/pouli%C4%8Dn%C3%AD-j%C3%ADdlo-burgery-z-masn%C3%BDch-%C5%99%C3%ADzk%C5%AF-jsou-v-pap%C3%ADrov%C3%BDch-krabic%C3%ADch-rozvoz-j%C3%ADdla.jpg?s=612x612&w=0&k=20&c=Gc_EU3B-uy9SPgpNfdh7TEm0awYIJPTGoV-UeodTQGo=",
-      "name": 'check ',
-      "country": 'usa'
-    },
-    {
-      "url":
-          "https://media.istockphoto.com/id/502840530/cs/fotografie/luxusn%C3%AD-restaura%C4%8Dn%C3%AD-st%C5%AFl-p%C5%99i-z%C3%A1padu-slunce.jpg?s=612x612&w=0&k=20&c=Sflhzn04I1xnVYecBfb8ukLVzO7lzL81CbO__4Xht7M=",
-      "name": 'check ',
-      "country": 'usa'
-    },
-    {
-      "url":
-          "https://media.istockphoto.com/id/1277719283/cs/fotografie/pouli%C4%8Dn%C3%AD-j%C3%ADdlo-burgery-z-masn%C3%BDch-%C5%99%C3%ADzk%C5%AF-jsou-v-pap%C3%ADrov%C3%BDch-krabic%C3%ADch-rozvoz-j%C3%ADdla.jpg?s=612x612&w=0&k=20&c=Gc_EU3B-uy9SPgpNfdh7TEm0awYIJPTGoV-UeodTQGo=",
-      "name": 'check ',
-      "country": 'usa'
-    },
-    {
-      "url":
-          'https://media.istockphoto.com/id/1271870386/cs/fotografie/arabsk%C3%A1-tradi%C4%8Dn%C3%AD-kuchyn%C4%9B-bl%C3%ADzkov%C3%BDchodn%C3%AD-meze-s-pitou-olivami-barevn%C3%BDm-hummusem-falafelem.jpg?s=612x612&w=0&k=20&c=mBvhoOjo2n-wbHbWYKBgF5K2Halb_IuL569cqxyMSqQ=',
-      "name": 'check ',
-      "country": 'usa'
-    },
+  List<Widget> foodList = [
+    ImageCard(
+        url:
+            'https://docs.flutter.dev/cookbook/img-files/effects/parallax/01-mount-rushmore.jpg',
+        name: "Mount Rushmore",
+        country: "U.S.A"),
+    ImageCard(
+        url:
+            'https://docs.flutter.dev/cookbook/img-files/effects/parallax/02-singapore.jpg',
+        name: "Gardens By The Bay",
+        country: "Singapore"),
+    ImageCard(
+        url:
+            'https://docs.flutter.dev/cookbook/img-files/effects/parallax/03-machu-picchu.jpg',
+        name: "Machu Picchu",
+        country: "Peru"),
+    ImageCard(
+      name: 'Vitznau',
+      country: 'Switzerland',
+      url:
+          'https://docs.flutter.dev/cookbook/img-files/effects/parallax/04-vitznau.jpg',
+    ),
+    ImageCard(
+      name: 'Bali',
+      country: 'Indonesia',
+      url:
+          'https://docs.flutter.dev/cookbook/img-files/effects/parallax/05-bali.jpg',
+    ),
+    ImageCard(
+      name: 'Mexico City',
+      country: 'Mexico',
+      url:
+          'https://docs.flutter.dev/cookbook/img-files/effects/parallax/06-mexico-city.jpg',
+    ),
+    ImageCard(
+      name: 'Cairo',
+      country: 'Egypt',
+      url:
+          'https://docs.flutter.dev/cookbook/img-files/effects/parallax/07-cairo.jpg',
+    ),
   ];
-
-  List<Widget> renderImageCard() {
-    List<Widget> list = [];
-    for (var element in foodList) {
-      list.add(ImageCard(
-          url: element['url'],
-          name: element['name'],
-          country: element['country']));
-    }
-    return list;
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: renderImageCard(),
-          ),
+    return Center(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: foodList,
         ),
       ),
     );
